9a6aeac2783fd52e0be3d4d14f397f71c5a6eb03
zhfei <1174977961@qq.com>
2023-06-06 23:29:29 +0800

    feat: 完善自定义气泡弹窗

--- BubblePopup/ViewController.swift
+++ BubblePopup/ViewController.swift
@@ -8,9 +8,17 @@
 import UIKit
  
 class ViewController: UIViewController {
+    
+    enum BubblePopupType: Int {
+        case dotline = 11
+        case triangle
+        case custome
+        
+    }
 
     @IBOutlet weak var btns: UIStackView!
     private var isDotLine = false
+    private var showType: BubblePopupType = .dotline
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,41 +29,62 @@ class ViewController: UIViewController {
 
      
     @IBAction func showPopup(_ sender: UIButton) {
-        let myContentView = CustomContentView()
-        
-        BubblePopupManager.shared.addPopup(toView: self.view, customContentView: myContentView, popupType: .dotLine, positionType: .bottom, popupPoint: nil, linkPoint: CGPoint(x: sender.frame.midX, y: sender.frame.minY), maxWidth: 200.0)
-        return
-        
-        if isDotLine {
-            //虚线气泡
-            switch sender.tag {
-            case 1:
-                BubblePopupManager.shared.addPopup(toView: self.view, tips: "冒泡弹窗", popupType: .dotLine, positionType: .bottom, popupPoint: nil, linkPoint: CGPoint(x: sender.frame.midX, y: sender.frame.minY), maxWidth: 200.0)
-            case 2:
-                BubblePopupManager.shared.addPopup(toView: self.view, tips: "冒泡弹窗", popupType: .dotLine, positionType: .top, popupPoint: nil, linkPoint: CGPoint(x: sender.frame.midX, y: sender.frame.maxY), maxWidth: 200.0)
-            case 3:
-                BubblePopupManager.shared.addPopup(toView: self.view, tips: "冒泡弹窗", popupType: .dotLine, positionType: .left, popupPoint: nil, linkPoint: CGPoint(x: sender.frame.minX, y: sender.frame.midY), maxWidth: 200.0)
-            case 4:
-                BubblePopupManager.shared.addPopup(toView: self.view, tips: "冒泡弹窗", popupType: .dotLine, positionType: .right, popupPoint: nil, linkPoint: CGPoint(x: sender.frame.maxX, y: sender.frame.midY), maxWidth: 200.0)
-            default:
-                break
-            }
-        } else {
-            //三角
-            switch sender.tag {
-            case 1:
-                BubblePopupManager.shared.addPopup(toView: self.view, tips: "冒泡弹窗", popupType: .triangle, positionType: .bottom, popupPoint: CGPoint(x: sender.frame.midX, y: sender.frame.minY), linkPoint: nil, maxWidth: 200.0)
-            case 2:
-                BubblePopupManager.shared.addPopup(toView: self.view, tips: "冒泡弹窗", popupType: .triangle, positionType: .top, popupPoint: CGPoint(x: sender.frame.midX, y: sender.frame.maxY), linkPoint: nil, maxWidth: 200.0)
-            case 3:
-                BubblePopupManager.shared.addPopup(toView: self.view, tips: "冒泡弹窗", popupType: .triangle, positionType: .left, popupPoint: CGPoint(x: sender.frame.minX, y: sender.frame.midY), linkPoint: nil, maxWidth: 200.0)
-            case 4:
-                BubblePopupManager.shared.addPopup(toView: self.view, tips: "冒泡弹窗", popupType: .triangle, positionType: .right, popupPoint: CGPoint(x: sender.frame.maxX, y: sender.frame.midY), linkPoint: nil, maxWidth: 200.0)
-            default:
-                break
-            }
+        switch showType {
+        case .dotline:
+            showDotLineBubblePopup(sender: sender)
+        case .triangle:
+            showTriangleBubblePopup(sender: sender)
+        case .custome:
+            showCustomContentBubblePopup(sender: sender)
         }
- 
+    }
+    
+    func showDotLineBubblePopup(sender: UIButton) {
+        switch sender.tag {
+        case 1:
+            BubblePopupManager.shared.addPopup(toView: self.view, tips: "冒泡弹窗", popupType: .dotLine, positionType: .bottom, popupPoint: nil, linkPoint: CGPoint(x: sender.frame.midX, y: sender.frame.minY), maxWidth: 200.0)
+        case 2:
+            BubblePopupManager.shared.addPopup(toView: self.view, tips: "冒泡弹窗", popupType: .dotLine, positionType: .top, popupPoint: nil, linkPoint: CGPoint(x: sender.frame.midX, y: sender.frame.maxY), maxWidth: 200.0)
+        case 3:
+            BubblePopupManager.shared.addPopup(toView: self.view, tips: "冒泡弹窗", popupType: .dotLine, positionType: .left, popupPoint: nil, linkPoint: CGPoint(x: sender.frame.minX, y: sender.frame.midY), maxWidth: 200.0)
+        case 4:
+            BubblePopupManager.shared.addPopup(toView: self.view, tips: "冒泡弹窗", popupType: .dotLine, positionType: .right, popupPoint: nil, linkPoint: CGPoint(x: sender.frame.maxX, y: sender.frame.midY), maxWidth: 200.0)
+        default:
+            break
+        }
+    }
+    
+    func showTriangleBubblePopup(sender: UIButton) {
+        switch sender.tag {
+        case 1:
+            BubblePopupManager.shared.addPopup(toView: self.view, tips: "冒泡弹窗", popupType: .triangle, positionType: .bottom, popupPoint: CGPoint(x: sender.frame.midX, y: sender.frame.minY), linkPoint: nil, maxWidth: 200.0)
+        case 2:
+            BubblePopupManager.shared.addPopup(toView: self.view, tips: "冒泡弹窗", popupType: .triangle, positionType: .top, popupPoint: CGPoint(x: sender.frame.midX, y: sender.frame.maxY), linkPoint: nil, maxWidth: 200.0)
+        case 3:
+            BubblePopupManager.shared.addPopup(toView: self.view, tips: "冒泡弹窗", popupType: .triangle, positionType: .left, popupPoint: CGPoint(x: sender.frame.minX, y: sender.frame.midY), linkPoint: nil, maxWidth: 200.0)
+        case 4:
+            BubblePopupManager.shared.addPopup(toView: self.view, tips: "冒泡弹窗", popupType: .triangle, positionType: .right, popupPoint: CGPoint(x: sender.frame.maxX, y: sender.frame.midY), linkPoint: nil, maxWidth: 200.0)
+        default:
+            break
+        }
+    }
+    
+    func showCustomContentBubblePopup(sender: UIButton) {
+        let myContentView = CustomContentView()
+        switch sender.tag {
+        case 1:
+            BubblePopupManager.shared.addPopup(toView: self.view, customContentView: myContentView, popupType: .dotLine, positionType: .bottom, popupPoint: nil, linkPoint: CGPoint(x: sender.frame.midX, y: sender.frame.minY), maxWidth: 200.0)
+        case 2:
+            
+            BubblePopupManager.shared.addPopup(toView: self.view, customContentView: myContentView, popupType: .dotLine, positionType: .top, popupPoint: nil, linkPoint: CGPoint(x: sender.frame.midX, y: sender.frame.maxY), maxWidth: 200.0)
+        case 3:
+            BubblePopupManager.shared.addPopup(toView: self.view, customContentView: myContentView, popupType: .dotLine, positionType: .left, popupPoint: nil, linkPoint: CGPoint(x: sender.frame.minX, y: sender.frame.midY), maxWidth: 200.0)
+        case 4:
+            BubblePopupManager.shared.addPopup(toView: self.view, customContentView: myContentView, popupType: .dotLine, positionType: .right, popupPoint: nil, linkPoint: CGPoint(x: sender.frame.maxX, y: sender.frame.midY), maxWidth: 200.0)
+        default:
+            break
+        }
+        
     }
     
     @IBAction func switchAction(_ sender: UIButton) {
@@ -65,7 +94,7 @@ class ViewController: UIViewController {
             }
         }
         sender.isEnabled = false
-        isDotLine = (sender.tag == 12)
+        showType = BubblePopupType(rawValue: sender.tag)!
     }
     
     
