9252ccb80eba0ffd3d79a9b0cc2826972cf1750d
zhfei <1174977961@qq.com>
2023-06-08 22:36:24 +0800

    other: 添加注释

--- BubblePopup/SRC/BubblePopupManager.swift
+++ BubblePopup/SRC/BubblePopupManager.swift
@@ -9,10 +9,18 @@ import UIKit
 
 @objcMembers
 class BubblePopupManager: NSObject {
-     
     static let shared = BubblePopupManager()
     private var closeCallBack: (() -> Void)?
-     
+    
+    /// 添加文字型气泡提示
+    /// - Parameters:
+    ///   - toView: 要添加到的View
+    ///   - tips: 字符串提示
+    ///   - popupType: 气泡类型：三角形or虚线型
+    ///   - positionType: 气泡类型标志（三角形or虚线型）的位置
+    ///   - popupPoint: 三角形标志指示的点
+    ///   - linkPoint: 虚线标志连接的点
+    ///   - maxWidth: 弹窗的最大宽度，用来设置计算字符串型弹窗
     func addPopup(toView: UIView, tips: String, popupType:BubblePopupType, positionType: BubblePopupPositionType, popupPoint: CGPoint?, linkPoint: CGPoint?, maxWidth: Double) {
          
         var popup: BubblePopup?;
@@ -48,6 +56,15 @@ class BubblePopupManager: NSObject {
         }
     }
      
+    /// 添加文字型气泡提示
+    /// - Parameters:
+    ///   - toView: 要添加到的View
+    ///   - customContentView: 自定义View，注意：里面的子视图的位置不能使用约束布局
+    ///   - popupType: 气泡类型：三角形or虚线型
+    ///   - positionType: 气泡类型标志（三角形or虚线型）的位置
+    ///   - popupPoint: 三角形标志指示的点
+    ///   - linkPoint: 虚线标志连接的点
+    ///   - maxWidth: 弹窗的最大宽度，用来设置计算字符串型弹窗
     func addPopup(toView: UIView, customContentView: UIView, popupType:BubblePopupType, positionType: BubblePopupPositionType, popupPoint: CGPoint?, linkPoint: CGPoint?, maxWidth: Double) {
               
             var popup: BubblePopup?;
